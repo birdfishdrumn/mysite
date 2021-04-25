c020f9a38deee30cc4590ecad16d332247ba20a0
michihiro <pomeofishbird0303@gmail.com>
2021-04-25 17:28:39 +0900

    修正

---
+++ src/components/dist/index.dev.js
@@ -0,0 +1,184 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _exportNames = {
+  Product: true,
+  WorkshopTab: true,
+  ProductCard: true,
+  WindBellCard: true,
+  Contact: true,
+  FloatingAction: true,
+  Access: true,
+  QuestionAccordion: true,
+  Process: true,
+  ProductTab: true,
+  Map: true,
+  Pickup: true,
+  TopPageContent: true,
+  WorkshopList: true,
+  Reserve: true,
+  IntroduceCircle: true,
+  TimeLine: true,
+  COVID_19: true
+};
+Object.defineProperty(exports, "Product", {
+  enumerable: true,
+  get: function get() {
+    return _Product["default"];
+  }
+});
+Object.defineProperty(exports, "WorkshopTab", {
+  enumerable: true,
+  get: function get() {
+    return _WorkshopTab["default"];
+  }
+});
+Object.defineProperty(exports, "ProductCard", {
+  enumerable: true,
+  get: function get() {
+    return _ProductCard["default"];
+  }
+});
+Object.defineProperty(exports, "WindBellCard", {
+  enumerable: true,
+  get: function get() {
+    return _WindBellCard["default"];
+  }
+});
+Object.defineProperty(exports, "Contact", {
+  enumerable: true,
+  get: function get() {
+    return _contact["default"];
+  }
+});
+Object.defineProperty(exports, "FloatingAction", {
+  enumerable: true,
+  get: function get() {
+    return _FloatingAction["default"];
+  }
+});
+Object.defineProperty(exports, "Access", {
+  enumerable: true,
+  get: function get() {
+    return _Access["default"];
+  }
+});
+Object.defineProperty(exports, "QuestionAccordion", {
+  enumerable: true,
+  get: function get() {
+    return _QuestionAccordion["default"];
+  }
+});
+Object.defineProperty(exports, "Process", {
+  enumerable: true,
+  get: function get() {
+    return _Process["default"];
+  }
+});
+Object.defineProperty(exports, "ProductTab", {
+  enumerable: true,
+  get: function get() {
+    return _ProductTab["default"];
+  }
+});
+Object.defineProperty(exports, "Map", {
+  enumerable: true,
+  get: function get() {
+    return _Map["default"];
+  }
+});
+Object.defineProperty(exports, "Pickup", {
+  enumerable: true,
+  get: function get() {
+    return _Pickup["default"];
+  }
+});
+Object.defineProperty(exports, "TopPageContent", {
+  enumerable: true,
+  get: function get() {
+    return _TopPageContent["default"];
+  }
+});
+Object.defineProperty(exports, "WorkshopList", {
+  enumerable: true,
+  get: function get() {
+    return _WorkshopList["default"];
+  }
+});
+Object.defineProperty(exports, "Reserve", {
+  enumerable: true,
+  get: function get() {
+    return _Reserve["default"];
+  }
+});
+Object.defineProperty(exports, "IntroduceCircle", {
+  enumerable: true,
+  get: function get() {
+    return _IntroduceCircle["default"];
+  }
+});
+Object.defineProperty(exports, "TimeLine", {
+  enumerable: true,
+  get: function get() {
+    return _TimeLine["default"];
+  }
+});
+Object.defineProperty(exports, "COVID_19", {
+  enumerable: true,
+  get: function get() {
+    return _COVID_["default"];
+  }
+});
+
+var _Product = _interopRequireDefault(require("./Product/Product"));
+
+var _Logo = require("./Logo");
+
+Object.keys(_Logo).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Logo[key];
+    }
+  });
+});
+
+var _WorkshopTab = _interopRequireDefault(require("./TabMenu/WorkshopTab"));
+
+var _ProductCard = _interopRequireDefault(require("./ProductCard/ProductCard"));
+
+var _WindBellCard = _interopRequireDefault(require("./WindBellList/WindBellCard"));
+
+var _contact = _interopRequireDefault(require("./Contact/contact"));
+
+var _FloatingAction = _interopRequireDefault(require("./FloatingAction/FloatingAction"));
+
+var _Access = _interopRequireDefault(require("./PageComponents/Access"));
+
+var _QuestionAccordion = _interopRequireDefault(require("./QuestionAccordion/QuestionAccordion"));
+
+var _Process = _interopRequireDefault(require("./Process/Process"));
+
+var _ProductTab = _interopRequireDefault(require("./TabMenu/ProductTab"));
+
+var _Map = _interopRequireDefault(require("./Map/Map"));
+
+var _Pickup = _interopRequireDefault(require("./Pickup/Pickup"));
+
+var _TopPageContent = _interopRequireDefault(require("./TopPageContent/TopPageContent"));
+
+var _WorkshopList = _interopRequireDefault(require("./WorkshopList/WorkshopList"));
+
+var _Reserve = _interopRequireDefault(require("./Reserve/Reserve"));
+
+var _IntroduceCircle = _interopRequireDefault(require("./IntroduceCircle/IntroduceCircle"));
+
+var _TimeLine = _interopRequireDefault(require("./TimeLine/TimeLine"));
+
+var _COVID_ = _interopRequireDefault(require("./COVID_19/COVID_19"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,39 @@
-export { default as Product } from "./Product/Product"
+export { default as Product }
+from "./Product/Product"
 export * from "./Logo"
-export { default as WorkshopTab } from "./TabMenu/WorkshopTab"
-// export { default as CarouselSlider }
-// from "./CarouselSlider/CarouselSlider"
-export { default as ProductCard } from "./ProductCard/ProductCard"
-export { default as WindBellCard } from "./WindBellList/WindBellCard"
-export { default as Contact } from "./Contact/contact"
-export { default as FloatingAction } from "./FloatingAction/FloatingAction"
-export { default as Access } from "./PageComponents/Access"
-export { default as QuestionAccordion } from "./QuestionAccordion/QuestionAccordion"
-export { default as Process } from "./Process/Process"
-export { default as ProductTab } from "./TabMenu/ProductTab"
-export { default as Map } from "./Map/Map"
-export { default as Pickup } from "./Pickup/Pickup"
-export { default as TopPageContent } from "./TopPageContent/TopPageContent"
-export { default as WorkshopList } from "./WorkshopList/WorkshopList"
-export { default as Reserve } from "./Reserve/Reserve"
-export { default as IntroduceCircle } from "./IntroduceCircle/IntroduceCircle"
-export { default as TimeLine } from "./TimeLine/TimeLine"
-export { default as COVID_19 } from "./COVID_19/COVID_19"
-
-export { default as Instagram } from "./Instagram/Instagram"
+export { default as WorkshopTab }
+from "./TabMenu/WorkshopTab"
+    // export { default as CarouselSlider }
+    // from "./CarouselSlider/CarouselSlider"
+export { default as ProductCard }
+from "./ProductCard/ProductCard"
+export { default as WindBellCard }
+from "./WindBellList/WindBellCard"
+export { default as Contact }
+from "./Contact/contact"
+export { default as FloatingAction }
+from "./FloatingAction/FloatingAction"
+export { default as Access }
+from "./PageComponents/Access"
+export { default as QuestionAccordion }
+from "./QuestionAccordion/QuestionAccordion"
+export { default as Process }
+from "./Process/Process"
+export { default as ProductTab }
+from "./TabMenu/ProductTab"
+export { default as Map }
+from "./Map/Map"
+export { default as Pickup }
+from "./Pickup/Pickup"
+export { default as TopPageContent }
+from "./TopPageContent/TopPageContent"
+export { default as WorkshopList }
+from "./WorkshopList/WorkshopList"
+export { default as Reserve }
+from "./Reserve/Reserve"
+export { default as IntroduceCircle }
+from "./IntroduceCircle/IntroduceCircle"
+export { default as TimeLine }
+from "./TimeLine/TimeLine"
+export { default as COVID_19 }
+from "./COVID_19/COVID_19"
